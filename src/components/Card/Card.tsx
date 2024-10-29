562016df4fe256762dfac82b3098f8dc0868d174
Evgenij Razzhivalov <evgenii.razzhivalov@inrecolan.com>
2024-10-29 12:31:44 +0300

    refactor card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,38 +1,38 @@
-import { Card } from "antd";
+import { Card, Skeleton } from "antd";
 import { IGame } from "../../interfaces/games";
 import { CSS } from "@dnd-kit/utilities";
 import { useSortable } from "@dnd-kit/sortable";
 import styled from "styled-components";
-const StyleCoverImage = styled.div<{$name: string}>`
-      position:relative;
-      display:block;
-      overflow:hidden;
-      img{
-        transition: filter 0.3s ease;
-      }
-      &:hover img {
-        filter: brightness(50%)
-      }
-      &::before{
-        content: '${props => props.$name.replace(/'/g," ")}';
-        position:absolute;
-        top:50%;
-        left:50%;
-        transform: translate(-50%,-50%);
-        color:white;
-        opacity:0;
-        pointer-events: none;
-        z-index:999;
-        text-overflow: ellipsis;
-        max-height:65px;
-        overflow:hidden;
-      }
-      &:hover{
-        &:before{
-          opacity: 1;
-        }
-      }
-  `
+const StyleCoverImage = styled.div<{ $name: string }>`
+  position: relative;
+  display: block;
+  overflow: hidden;
+  img {
+    transition: filter 0.3s ease;
+  }
+  &:hover img {
+    filter: brightness(50%);
+  }
+  &::before {
+    content: "${(props) => props.$name.replace(/'/g, " ")}";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    opacity: 0;
+    pointer-events: none;
+    z-index: 999;
+    text-overflow: ellipsis;
+    max-height: 65px;
+    overflow: hidden;
+  }
+  &:hover {
+    &:before {
+      opacity: 1;
+    }
+  }
+`;
 export const CardGame: React.FC<{
   game?: IGame;
   loading: boolean;
@@ -48,7 +48,7 @@ export const CardGame: React.FC<{
   } = useSortable({
     id: id,
   });
-  const style = {
+  const style: React.CSSProperties = {
     transform: CSS.Translate.toString(transform),
     transition,
     opacity: isDragging ? "0.5" : "1",
@@ -59,34 +59,30 @@ export const CardGame: React.FC<{
     maxWidth: "130px",
     border: "none",
   };
-  
 
   return (
-    <Card
-      key={id}
-      loading={loading}
-      style={style}
-      styles={{
-        body: {
-          display: "none",
-        },
-      }}
-      ref={setNodeRef}
-      {...listeners}
-      {...attributes}
-      hoverable
-      cover={
-        !game || game === undefined ? null : (
-          <StyleCoverImage $name={game.name}>
+    <>
+      {loading ? (
+        <Skeleton.Image active style={style} />
+      ) : (
+        game && (
+          <StyleCoverImage
+            $name={game.name}
+            ref={setNodeRef}
+            {...listeners}
+            {...attributes}
+            key={id}
+            style={style}
+          >
             <img
-              style={{ objectFit: "cover",width:"100%" }}
+              style={{ objectFit: "cover", width: "100%" }}
               height={"200vh"}
               alt={game.name}
               src={game.background_image}
             />
           </StyleCoverImage>
         )
-      }
-    />
+      )}
+    </>
   );
 };
