2823cf585fea65376c08040eaa4b319bf1d29ef6
Evgenij Razzhivalov <evgenii.razzhivalov@inrecolan.com>
2024-11-05 13:40:23 +0300

    add user store

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,48 @@
 import styled from "styled-components";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { useEffect } from "react";
+import { resetAuthState, switchLoading } from "../../redux/slice/userSlice";
+import { Button } from "antd";
 
 const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.base.colors.bg};
   color: ${({ theme }) => theme.base.colors.font};
   height: 50px;
-  display:flex;
-  align-items:center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding: 0 10vw;
-  a{
-    color: ${({theme})=> theme.base.colors.font}
+  a {
+    color: ${({ theme }) => theme.base.colors.font};
   }
 `;
-export const Header = () => (
-  <StyledHeader>
-    GameCritic
-    <div>
-
-    </div>
-  </StyledHeader>
-);
+export const Header = () => {
+  const { username, loading } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      dispatch(switchLoading());
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  }, [dispatch]);
+  return (
+    <StyledHeader>
+      GameCritic
+      {loading || !username ? (
+        <div style={{ display: "flex", gap: "1vw" }}>
+          <Button>Регистрация</Button>
+          <Button>Авторизация</Button>
+        </div>
+      ) : (
+        username && (
+          <div style={{ display: "flex", gap: "1vw", alignItems: "center" }}>
+            Привет, {username}!
+            <Button danger onClick={() => dispatch(resetAuthState())}>
+              Выйти
+            </Button>
+          </div>
+        )
+      )}
+    </StyledHeader>
+  );
+};
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slice/themeSlice"
-
+import userReducer from './slice/userSlice'
 export const store = configureStore({
     reducer:{
         theme: themeReducer,
+        user:userReducer
     }
 })
 
