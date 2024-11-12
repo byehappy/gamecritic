743dc53223ec0f7b46c1dbd97b607098fefa9f04
Evgenij Razzhivalov <evgenii.razzhivalov@inrecolan.com>
2024-11-12 11:29:56 +0300

    add refresh interceptor

--- src/axios/requests/gamecriticAPI/auth.requests.ts
+++ src/axios/requests/gamecriticAPI/auth.requests.ts
@@ -32,3 +32,42 @@ export const getCurrentUser = () => {
   const user = localStorage.getItem("user");
   if (user) return JSON.parse(user);
 };
+
+instanceAPI.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    const originalConfig = err.config;
+
+    if (err.response) {
+      if (err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+        try {
+          const rs = await refreshToken();
+          const { accessToken } = rs.data;
+          localStorage.setItem("accessToken", accessToken);
+          return instanceAPI(originalConfig);
+        } catch (_error) {
+          if (_error.response && _error.response.data) {
+            return Promise.reject(_error.response.data);
+          }
+
+          return Promise.reject(_error);
+        }
+      }
+
+      if (err.response.status === 403 && err.response.data) {
+        return Promise.reject(err.response.data);
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+const refreshToken = () => {
+  return instanceAPI.post("/refresh", {
+    refreshToken: localStorage.getItem("refreshToken"),
+  });
+};
